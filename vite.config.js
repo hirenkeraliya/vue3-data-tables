7132b0aaa2de11476fcf1f5b5f624dc5b726d124
Hiren <hiren@freshbits.in>
2023-05-22 16:38:15 +0530

    Update Settings

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,20 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    lib: {
+      entry: 'src/main.js',
+      name: 'Vue3DataTable',
+      fileName: (format) => `vue3-data-tables.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        exports: "named",
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
 })
